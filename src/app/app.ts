0b6e363f5d9bf80498968f9fb9c8be202464fcde
gamma <santiagogamarrasanchez@gmail.com>
2025-10-09 23:50:03 -0500

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    MatButtonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
