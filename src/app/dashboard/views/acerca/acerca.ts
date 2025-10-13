46986e6929dbd3ae95f81299bf4bffaa81252120
gamma <santiagogamarrasanchez@gmail.com>
2025-10-13 15:00:27 -0500

    Acerca de (update) + img

--- src/app/dashboard/views/acerca/acerca.ts
+++ src/app/dashboard/views/acerca/acerca.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-acerca',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './acerca.html',
   styleUrl: './acerca.css'
 })
 export class Acerca {
-
+  equipo = [
+    { nombre: 'Santiago Gamarra', cargo: 'Director de Proyecto - CEO', foto: '/assets/img/foto-perfil.jpeg' }
+  ];
 }
